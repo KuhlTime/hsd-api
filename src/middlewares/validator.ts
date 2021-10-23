2bf92fae28e1aa4559e5965aae8d3aafe99622aa
André Kuhlmann <akuhltime@gmail.com>
2021-10-23 19:20:22 +0200

    Add validator middleware
    This middleware can be used to validate either the query or the body of a request.

---
+++ src/middlewares/validator.ts
@@ -0,0 +1,24 @@
+/* eslint-disable sonarjs/no-identical-functions */
+
+import { NextFunction, Request, Response } from 'express'
+import httpStatus from 'http-status'
+import { Schema } from 'joi'
+import APIError from '@/models/APIError'
+
+const query =
+  (schema: Schema) =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    const { error } = schema.validate(req.query)
+    if (error) next(new APIError(httpStatus.BAD_REQUEST, error.message))
+    else next()
+  }
+
+const body =
+  (schema: Schema) =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    const { error } = schema.validate(req.query)
+    if (error) next(new APIError(httpStatus.BAD_REQUEST, error.message))
+    else next()
+  }
+
+export { query, body }
