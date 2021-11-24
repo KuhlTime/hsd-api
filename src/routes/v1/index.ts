5a0e548cec16eccb819740d526c1229d608c0c26
André Kuhlmann <akuhltime@gmail.com>
2021-11-24 20:59:11 +0100

    Change route information

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -12,17 +12,8 @@ router.use('/', publicRouter)
 
 router.get('/', (req: Request, res: Response) => {
   res.send({
-    message: 'Welcome to the hsd-api.',
-    github: 'https://github.com/KuhlTime/hsd-api',
-    data: {
-      courses: `${env.url}/v1/courses`,
-      degrees: `${env.url}/v1/degrees`,
-      exams: `${env.url}/v1/exams`
-    },
-    author: {
-      name: 'André Kuhlmann',
-      email: 'andre.kuhlmann@study.hs-duesseldorf.de',
-      links: ['https://kuhlti.me', 'https://github.com/KuhlTime']
+    routes: {
+      exams: `/v1/exams`
     }
   })
 })
